6b7d15e7276609cddabbbebce4b832d664673c8c
Yehor Lvivski <lvivski@gmail.com>
2015-08-16 13:51:33 +0300

    style

--- src/util.js
+++ src/util.js
@@ -6,7 +6,8 @@ function parse(obj) {
 	if  (obj.length === 1 && Array.isArray(obj[0])) {
 		return obj[0]
 	} else {
-		var args = new Array(obj.length), i = 0
+		var args = new Array(obj.length),
+			i = 0
 		while (i < args.length) {
 			args[i] = obj[i++]
 		}
